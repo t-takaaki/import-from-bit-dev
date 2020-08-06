264835835415dce804bf8737f1aabe4a644f60bf
t-takaaki <hokahoka.oden@gmail.com>
2020-08-06 20:21:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { render } from "react-dom"
+import Hello from "@bit/t-takaaki.sample01.hello"
+
+const root = document.getElementById("root")
+render(<Hoge />, root)
